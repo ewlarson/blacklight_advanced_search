fd23d9bd5f72ddd5532a67e11b785d1ed1b60ffc
Eric Larson <ewlarson@gmail.com>
2023-03-03 09:40:09 -0600

    Update render_constraints_override.rb
    Skip facet content build

--- lib/blacklight_advanced_search/render_constraints_override.rb
+++ lib/blacklight_advanced_search/render_constraints_override.rb
@@ -17,23 +17,24 @@ module BlacklightAdvancedSearch::RenderConstraintsOverride
       return super(my_params)
     else
       content = []
-      advanced_query.keyword_queries.each_pair do |field, query|
-        label = blacklight_config.search_fields[field][:label]
-        content << render_constraint_element(
-          label, query,
-          :remove =>
-            search_action_path(remove_advanced_keyword_query(field, my_params).except(:controller, :action))
-        )
-      end
-      if (advanced_query.keyword_op == "OR" &&
-          advanced_query.keyword_queries.length > 1)
-        content.unshift content_tag(:span, "Any of:", class: 'operator')
-        content_tag :span, class: "inclusive_or appliedFilter well" do
-          safe_join(content.flatten, "\n")
-        end
-      else
-        safe_join(content.flatten, "\n")
-      end
+      # EWL - Skip content build
+      #advanced_query.keyword_queries.each_pair do |field, query|
+      #  label = blacklight_config.search_fields[field][:label]
+      #  content << render_constraint_element(
+      #    label, query,
+      #    :remove =>
+      #      search_action_path(remove_advanced_keyword_query(field, my_params).except(:controller, :action))
+      #  )
+      #end
+      #if (advanced_query.keyword_op == "OR" &&
+      #    advanced_query.keyword_queries.length > 1)
+      #  content.unshift content_tag(:span, "Any of:", class: 'operator')
+      #  content_tag :span, class: "inclusive_or appliedFilter well" do
+      #    safe_join(content.flatten, "\n")
+      #  end
+      #else
+      #  safe_join(content.flatten, "\n")
+      #end
     end
   end
 
